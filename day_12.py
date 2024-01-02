555b7a395815b50260a7cfccb85abbe07193cf89
user <user@mail.com>
2024-01-02 19:47:31 +0100

    Part 1 done

---
+++ day_12.py
@@ -0,0 +1,52 @@
+file_path = 'input.txt'
+lines = []
+
+try:
+    with open(file_path, 'r') as file:
+        for line in file:
+            lines.append(line.strip())
+except FileNotFoundError:
+    print(f"The file {file_path} does not exist.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+records = []
+for line in lines :
+    springs, groups = line.split(' ')
+    groups = [int(c) for c in groups.split(',')]
+    records.append((springs, groups))
+
+
+def is_valid(springs, groups):
+    if len(groups) == 0:
+        return '#' not in springs
+
+    if len(springs) == 0 and len(groups) == 0:
+        return 1
+
+    group_len = groups[0]
+    min_length = sum(groups) + len(groups) - 1
+    if len(springs) < min_length:
+        return 0
+
+    arrangements = 0
+    if springs[0] == '.':
+        return is_valid(springs[1:], groups)
+
+    if '.' not in springs[:group_len]:
+        if len(springs) == group_len or len(springs) > group_len and springs[group_len] != "#":
+            arrangements += is_valid(springs[group_len + 1:], groups[1:])
+
+    if springs[0] == '?':
+        arrangements += is_valid(springs[1:], groups)
+
+    return arrangements
+
+def part_1():
+    total = 0
+    for record in records:
+        springs, groups = record
+        total += is_valid(springs, groups)
+    print(total)
+
+part_1()
