90eb90507e6e74ebd2b4f8294b180497540aceee
user <user@mail.com>
2024-01-24 17:28:29 +0100

    Part 2 done

--- day_19.py
+++ day_19.py
@@ -1,4 +1,5 @@
 import re
+import copy
 
 file_path = 'input.txt'
 with open(file_path, 'r') as file:
@@ -45,7 +46,6 @@ def get_score():
 
         while current_workflow != 'A' and current_workflow != 'R':
             current_list = workflows_dic[current_workflow]
-            print(current_workflow, current_list)
             for key, val in current_list:
                 if val == '-1':
                     current_workflow = key
@@ -54,16 +54,13 @@ def get_score():
                     part_value = part_dic[key]
                     criteria, destination = val.split(':')
                     if '<' in criteria:
-                        print(part_value, criteria, destination)
                         if part_value < int(criteria[1:]):
                             current_workflow = destination
                             break
                     elif '>' in criteria:
-                        print(part_value, criteria, destination)
                         if part_value > int(criteria[1:]):
                             current_workflow = destination
                             break
-        print(current_workflow, '\n')
 
         if current_workflow == 'A':
             score += sum(part_dic.values())
@@ -71,4 +68,60 @@ def get_score():
 
 def part_1():
     print(get_score())
-part_1()
+#part_1()
+
+
+def get_combinations():
+    workflows_dic = build_workflows_dic(workflows)
+    possible_values = {'x': [1, 4000], 'm': [1, 4000], 'a': [1, 4000], 's': [1, 4000]}
+    current_workflow = 'in'
+    path_queue = [(current_workflow, possible_values)]
+    valid_paths = []
+    while path_queue:
+        workflow_name, possible_values = path_queue.pop(0)
+        for key, val in workflows_dic[workflow_name]:
+            if val == '-1':
+                if key == 'A':
+                    valid_paths.append(copy.deepcopy(possible_values))
+                    break
+                elif key != 'R':
+                    path_queue.append((key, copy.deepcopy(possible_values)))
+            else:
+                criteria, destination = val.split(':')
+                if '<' in criteria:
+                    valid_criteria_values = copy.deepcopy(possible_values)
+                    valid_criteria_values[key][1] = int(criteria[1:]) - 1
+                    possible_values[key][0] = int(criteria[1:])
+
+                    if destination == 'A':
+                        valid_paths.append(valid_criteria_values)
+                    elif destination != 'R':
+                        possible_values[key][0] = int(criteria[1:])
+                        path_queue.append((destination, valid_criteria_values))
+
+                elif '>' in criteria:
+                    valid_criteria_values = copy.deepcopy(possible_values)
+                    valid_criteria_values[key][0] = int(criteria[1:]) + 1
+                    possible_values[key][1] = int(criteria[1:])
+                    if destination == 'A':
+                        valid_paths.append(valid_criteria_values)
+                    elif destination != 'R':
+                        path_queue.append((destination, copy.deepcopy(valid_criteria_values)))
+    return get_score(valid_paths)
+
+
+def get_score(valid_paths):
+    total = 0
+    for path in valid_paths:
+        combinations = 1
+        for min_val, max_val in path.values():
+            print(min_val, max_val)
+            combinations *= max_val - min_val + 1
+        total += combinations
+    return total
+
+
+def part_2():
+    print(get_combinations())
+
+part_2()
