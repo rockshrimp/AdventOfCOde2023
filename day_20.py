90fe4a794ebb988a154c52e3c28926b364e46c47
user <user@mail.com>
2024-02-07 00:03:54 +0100

    Part 2 done

--- day_20.py
+++ day_20.py
@@ -1,18 +1,14 @@
+from copy import deepcopy
+from math import lcm
+
 file_path = 'input.txt'
 
-def state_to_tuple(state_dic):
-    state = []
-    for k, v in state_dic.items():
-        if k == 'flip_flops':
-            state.append(('flip_flops', tuple(sorted(v))))
-        else:
-            conjunctions = []
-            for conjunction, modules in v.items():
-                conjunctions.append((conjunction, tuple(modules)))
-
-            state.append(('conjunction', tuple(sorted(conjunctions))))
-    return tuple(state)
-
+def get_predecessors(module):
+    pred = []
+    for source, destinations in signals_dic.items():
+        if module in destinations[1]:
+            pred.append(source)
+    return pred
 
 with open(file_path, 'r') as file:
     lines = file.read().splitlines()
@@ -49,8 +45,6 @@ for source, destinations in signals_dic.items():
             sorted(initial_state_dic['conjunctions'][dest]).append((source, 0))
 
 
-states_dic = {state_to_tuple(initial_state_dic): 0}
-
 def run_module_conf(signals_dic, flip_flops_state, conjunctions_state):
     low_pulses_count, high_pulses_count = 0, 0
     signals_queue = [('broadcaster', 0)]
@@ -69,7 +63,6 @@ def run_module_conf(signals_dic, flip_flops_state, conjunctions_state):
                 else:
                     low_pulses_count += 1
         elif signals_dic[source][0] == '%':
-            #print(source, pulse_value, signals_dic[source][0], signals_dic[source][1])
             if pulse_value == 0:
                 flip_flops_state[source] ^= 1
                 pulse_value = flip_flops_state[source]
@@ -92,22 +85,65 @@ def run_module_conf(signals_dic, flip_flops_state, conjunctions_state):
                     high_pulses_count += 1
                 else:
                     low_pulses_count += 1
-        #print(source, pulse_value, signals_dic[source][1], signals_queue, low_pulses_count, high_pulses_count)
     return low_pulses_count, high_pulses_count, flip_flops_state, conjunctions_state
 
 def part_1():
-    state_dic = initial_state_dic
+    state_dic = deepcopy(initial_state_dic)
     flip_flops_state, conjunctions_state = state_dic['flip_flops'], state_dic['conjunctions']
-    print('flip_flop_state', flip_flops_state)
-    print('conjunction_state', conjunctions_state)
     low_pulses_total, high_pulses_total = 0, 0
     for i in range(1000):
         low_pulses_count, high_pulses_count, flip_flops_state, conjunctions_state = run_module_conf(signals_dic, flip_flops_state, conjunctions_state)
         low_pulses_total += low_pulses_count + 1
         high_pulses_total += high_pulses_count
-        #print('\nround done')
-    print('low_pulses_total', low_pulses_total, 'high_pulses_total', high_pulses_total)
-        #print('flip_flop_state', flip_flops_state)
-        #print('conjunction_state', conjunctions_state)
-    print(low_pulses_total * high_pulses_total)
+    print('Part 1', high_pulses_total * low_pulses_total)
 part_1()
+
+
+def run_module_conf(predecessor, flip_flops_state, conjunctions_state, signals_queue):
+    signals_queue = [('broadcaster', 0)]
+    while signals_queue:
+        source, pulse_value = signals_queue.pop(0)
+        if source in non_sending_modules:
+            continue
+        if source == 'broadcaster':
+            for dest in signals_dic[source][1]:
+                signals_queue.append((dest, pulse_value))
+                if dest in conjunctions_state:
+                    conjunctions_state[dest][source] = pulse_value
+        elif signals_dic[source][0] == '%':
+            if pulse_value == 0:
+                flip_flops_state[source] ^= 1
+                pulse_value = flip_flops_state[source]
+                for dest in signals_dic[source][1]:
+                    signals_queue.append((dest, pulse_value))
+                    if dest in conjunctions_state:
+                        conjunctions_state[dest][source] = pulse_value
+        elif signals_dic[source][0] == '&':
+            connected_inputs = conjunctions_state[source]
+            pulse_value = 0 if all(connected_inputs.values()) else 1
+            if source == predecessor and pulse_value == 1:
+                return True, flip_flops_state, conjunctions_state
+            for dest in signals_dic[source][1]:
+                signals_queue.append((dest, pulse_value))
+                if dest in conjunctions_state:
+                    conjunctions_state[dest][source] = pulse_value
+    return False, flip_flops_state, conjunctions_state
+
+
+def part_2():
+    rx_predecessors = get_predecessors('rx')
+    predecessors_count = []
+    for predecessor in get_predecessors(*rx_predecessors):
+        counter = 0
+        signals_queue = [('broadcaster', 0)]
+        state_dic = deepcopy(initial_state_dic)
+        flip_flops_state, conjunctions_state = state_dic['flip_flops'], state_dic['conjunctions']
+
+        while True:
+            counter += 1
+            found, flip_flops_state, conjunctions_state = run_module_conf(predecessor, flip_flops_state, conjunctions_state, signals_queue)
+            if found:
+                predecessors_count.append(counter)
+                break
+    print(lcm(*predecessors_count))
+part_2()
