d18d2189f282820aeb543b54bf865401e8219b78
user <user@mail.com>
2024-02-13 22:26:37 +0100

    Part 2 done

--- day_22.py
+++ day_22.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from copy import deepcopy
 
 with open('input.txt', 'r') as file:
     lines = file.read().splitlines()
@@ -31,9 +32,9 @@ for brick_idx, brick in enumerate(bricks):
                 heights_under_brick.append(height)
             else:
                 heights_under_brick.append(0)
+
         for y in range(y_start, y_end + 1):
             max_height = max(heights_under_brick)
-
             if len(matrix[x_start][y]) > 0:
                 if matrix[x_start][y][-1]['height'] == max_height:
                     support[brick_idx].add(matrix[x_start][y][-1]['brick_idx'])
@@ -55,7 +56,6 @@ for brick_idx, brick in enumerate(bricks):
                     supported[matrix[x][y_start][-1]['brick_idx']].add(brick_idx)
             matrix[x][y_start].append({'height': max_height + 1, 'brick_idx': brick_idx})
     else:
-        print(z_start, z_end)
         if len(matrix[x_start][y_start]) > 0:
             height = matrix[x_start][y_start][-1]['height']
             support[brick_idx].add(matrix[x_start][y_start][-1]['brick_idx'])
@@ -66,15 +66,40 @@ for brick_idx, brick in enumerate(bricks):
             for z in range(z_start, z_end + 1):
                 matrix[x_start][y_start].append({'height': 0 + z - z_start + 1, 'brick_idx': brick_idx})
 
-print('supported', supported)
-print('support',support)
-
 
 def part_1():
     safe_to_disintegrate_count = 0
     for support_brick, supported_brick_set in supported.items():
         if all([len(support[supported_brick]) > 1 for supported_brick in supported_brick_set]):
             safe_to_disintegrate_count += 1
-            #print(support_brick, 'is safe to disintegrate')
     print(safe_to_disintegrate_count)
-part_1()
+#part_1()
+
+
+def get_falling_bricks(support_brick):
+    global falling_bricks_count
+    falling_bricks_per_brick = {}
+    queue = [support_brick]
+    fallen_bricks = {support_brick}
+
+    while queue:
+        brick = queue.pop(0)
+
+        for supported_brick in supported[brick]:
+            if supported_brick in fallen_bricks:
+                continue
+
+            queue.append(supported_brick)
+
+            if support[supported_brick].issubset(fallen_bricks):
+                falling_bricks_count += 1
+                fallen_bricks.add(supported_brick)
+
+falling_bricks_count = 0
+print(supported)
+def part_2():
+    for idx, support_brick in enumerate(supported):
+        print(idx, 'for brick :', support_brick)
+        get_falling_bricks(support_brick)
+    print(falling_bricks_count)
+part_2()
